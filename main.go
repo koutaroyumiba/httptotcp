1794334d9be35701c8c3f58761651acf8cd87923
Kot <89110272+koutaroyumiba@users.noreply.github.com>
2026-09-05 18:45:24 +1200

    feat: reading from that go channel

--- main.go
+++ main.go
@@ -15,32 +15,46 @@ func main() {
 	}
 	defer file.Close()
 
-	var currentLine strings.Builder
+	ch := getLinesChannel(file)
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
+	}
+}
 
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
-		if err != nil {
-			if err == io.EOF {
-				return
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer close(out)
+		var currentLine strings.Builder
+
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				if err == io.EOF {
+					return
+				}
+				log.Fatalf("Failed to read from file: %v", err)
+				break
 			}
-			log.Fatalf("Failed to read from file: %v", err)
-			break
-		}
 
-		parts := strings.Split(string(data[:n]), "\n")
-		if len(parts) == 2 {
-			// new line found
-			fmt.Printf("read: %s%s\n", currentLine.String(), parts[0])
-			currentLine.Reset()
-			currentLine.WriteString(parts[1])
-		} else if len(parts) == 1 {
-			currentLine.WriteString(parts[0])
-		} else {
-			log.Fatal("something is wrong?")
+			parts := strings.Split(string(data[:n]), "\n")
+			if len(parts) == 2 {
+				// new line found
+				out <- fmt.Sprintf("%s%s", currentLine.String(), parts[0])
+				currentLine.Reset()
+				currentLine.WriteString(parts[1])
+			} else if len(parts) == 1 {
+				currentLine.WriteString(parts[0])
+			} else {
+				log.Fatal("something is wrong?")
+			}
 		}
-	}
-	if currentLine.Len() != 0 {
-		fmt.Printf("read: %s\n", currentLine.String())
-	}
+		if currentLine.Len() != 0 {
+			out <- fmt.Sprintf("%s", currentLine.String())
+		}
+	}()
+
+	return out
 }
